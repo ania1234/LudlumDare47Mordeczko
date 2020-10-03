cefa689a75e33d82a916e2d82bbbdb5bbd515630
sarajujeczka <sarajujeczka@gmail.com>
2020-10-03 14:02:48 +0200

    inventory prefab added

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    
+    public static Inventory instance;
+
+    public int capacity = 10;
+
+    private List<ItemInfo> items = new List<ItemInfo>();
+
+    public void AddItem(ItemInfo item)
+    {
+        if (items.Count < capacity)
+        {
+            items.Add(item); 
+        }
+    }
+
+    public void RemoveItem(ItemInfo item)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+        }
+    }
 }
