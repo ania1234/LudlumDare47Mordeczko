06e7592591399bfad3920c7a71b5143acb715412
sarajujeczka <sarajujeczka@gmail.com>
2020-10-03 21:43:28 +0200

    new timer and hearts added

---
+++ Assets/Scripts/StatsUI/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Timer : MonoBehaviour
+{
+    Image image;
+
+    private IEnumerator Start()
+    {
+        while (GameManager.instance == null)
+        {
+            yield return null;
+        }
+
+        image = GetComponent<Image>();
+
+        GameManager.instance.onDayTimeChanged += OnDayTimeChanged;
+        GameManager.instance.onDayTimeLeftChanged += OnDayTimeLeftChanged;
+    }
+
+    private void OnDayTimeLeftChanged(float value)
+    {
+        image.fillAmount = Mathf.Lerp(0, 1, Mathf.InverseLerp(0, GameManager.instance.dayDuration, value));
+    }
+
+    private void OnDayTimeChanged(DayTimeEnum value)
+    {
+        image.fillAmount = 1;
+    }
+}
