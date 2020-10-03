974333f68d99db7c403dc8c1a5ea3779b1686578
sarajujeczka <sarajujeczka@gmail.com>
2020-10-03 11:38:07 +0200

    Inventory system - core

---
+++ Assets/ItemInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Data/New Item", order = 1)]
+public class ItemInfo : ScriptableObject
+{
+    public new string name;
+    public Sprite icon;
+}
