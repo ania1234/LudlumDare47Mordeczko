78d6995a713be57cb06638ca7332bcc6b7e6d7e0
sarajujeczka <sarajujeczka@gmail.com>
2020-10-04 20:31:05 +0200

    menu scene added, play button added

---
+++ Assets/Scripts/MenuPlay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class MenuPlay : MonoBehaviour
+{
+    public string sceneName = "SampleScene";
+
+    Button button;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Clicked);
+
+        mainCamera = Camera.main;
+    }
+
+    private void Clicked()
+    {
+        SceneManager.LoadScene(sceneName);
+
+        var dimCam = mainCamera.GetComponent<DimScript>();
+        dimCam.StopAllCoroutines();
+        dimCam.MoveDimValue(0, 1, 0.4f);
+    }
+}
