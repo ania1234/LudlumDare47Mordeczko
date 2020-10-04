2dbf21edfa625e4bd6427effd3aced3b7e016fe2
sarajujeczka <sarajujeczka@gmail.com>
2020-10-05 00:01:50 +0200

    win screen added

---
+++ Assets/Scripts/WinButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinButton : MonoBehaviour
+{
+    public string sceneName = "Menu";
+
+    Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Clicked);
+    }
+
+    private void Clicked()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
